545c7411bfe5e7c970069ad2897257363fd1d0f8
valentinbilot <valentin.bilot@gmail.com>
2018-11-30 20:33:15 +0100

    LSTM updates (ajout de l'accuracy)

--- exercice2/code_by_Dr_Tristan/utilities/chordUtil.py
+++ exercice2/code_by_Dr_Tristan/utilities/chordUtil.py
@@ -139,6 +139,7 @@ a2 = {
     'dim':     'dim',
     'sus4':    'N',
     'sus2':    'N',
+    '6':    '6', #add
     '7':       '7',
     'maj7':    'maj7',
     'min7':    'min7',
@@ -171,6 +172,7 @@ a3 = {
     'dim':     'dim',
     'sus4':    'sus',
     'sus2':    'sus',
+    '6':    '6', #add
     '7':       '7',
     'maj7':    'maj7',
     'min7':    'min7',
@@ -203,6 +205,7 @@ a5 = {
     'dim':     'dim',
     'sus4':    'sus4',
     'sus2':    'sus2',
+    '6':    '6', #add
     '7':       '7',
     'maj7':    'maj7',
     'min7':    'min7',
