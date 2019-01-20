3ed5741a8dde81e4d95c0b98dddf118d3667f09a
valentinbilot <valentin.bilot@gmail.com>
2019-01-20 07:34:24 +0100

    correction toyDataSet

--- ToyDataSet/easyToyDataSetGenerator.py
+++ ToyDataSet/easyToyDataSetGenerator.py
@@ -8,47 +8,61 @@ environment.set('midiPath', '/usr/bin/mscore')
 rootname = "clean_midi"
 filenames = os.listdir(rootname)
 
-
 for filename in filenames:
 
-    midifile = rootname + "/" + filename + "/" + \
-        os.listdir(rootname+"/"+filename)[0]
-    midiname = os.listdir(rootname+"/"+filename)[0]
-    littleMelody = converter.parse(midifile)
+	midifile = rootname + "/" + filename + "/" + os.listdir(rootname+"/"+filename)[0]
+	midiname =  os.listdir(rootname+"/"+filename)[0]
+	littleMelody = converter.parse(midifile)
 
-    chords = littleMelody.chordify()
+	chords = littleMelody.chordify()
 
-    chords = chords.elements
+	chords = chords.elements
 
-    listToWrite = []
+	listToWrite = []
 
-    time = 0
-    time_write = 0
-    for mychord in chords:
-        if type(mychord) != chord.Chord:
-            # do nothing
-            print("do nothing because it's type :", type(mychord))
-        elif mychord.isChord:
-            time += mychord.quarterLength
-            while time_write < time:
-                root = mychord.root().name
-                if root == "E-":
-                    root = "D#"
-                if root == "B-":
-                    root = "A#"
-                name = mychord.commonName
-                if name[0:5] == "minor":
-                    harmo = "min"
-                elif name[0:5] == "major":
-                    harmo = "maj"
-                else:
-                    harmo = "N"
-                if harmo == "N":
-                    listToWrite.append("N")
-                else:
-                    listToWrite.append(str(time_write)+" "+root+":"+harmo)
-                time_write += 1
 
-    with open('generated/'+midiname, 'w') as f:
-        for item in listToWrite:
-            f.write("%s\n" % item)
+	time = 0
+	time_write = 0
+	for mychord in chords:
+		if type(mychord) != chord.Chord:
+			#do nothing
+			print("doing nothing because it's type :", type(mychord))
+		elif mychord.isChord:
+			time+=mychord.quarterLength
+			while time_write < time :
+				root = mychord.root().name
+				if root == "E-":
+					root = "D#"
+				if root == "B-":
+					root = "A#"
+				name = mychord.commonName
+				if name[0:5] == "minor":
+					harmo = "min"
+				elif name[0:5] == "major":
+					harmo = "maj"
+				elif name[0:16] == "incomplete minor":
+					harmo = "min"
+				elif name[0:16] == "incomplete major":
+					harmo = "maj"
+				elif name == "incomplete dominant-seventh chord":
+					harmo = "maj"
+				elif name == "augmented major tetrachord":
+					harmo = "maj"
+				elif name[0:8] == "dominant":
+					harmo = "maj"
+				elif name == "perfect-fourth major tetrachord":
+					harmo = "maj"
+				else:
+					harmo = "N"
+				if harmo == "N":
+					listToWrite.append(str(time_write)+" "+"N")
+				else:
+					listToWrite.append(str(time_write)+" "+root+":"+harmo)
+				time_write+=1
+
+
+
+	with open('generated/'+midiname, 'w') as f:
+		for item in listToWrite:
+		    f.write("%s\n" % item)
+
