5f5d25536077435786e462553117a9a8e36c4fbf
valentinbilot <valentin.bilot@gmail.com>
2019-01-20 17:21:49 +0100

    Training on the ToyDataSet, too many 'N' :'(

--- SimpleRNNs/Results/REAMDE.md
+++
@@ -1,4 +0,0 @@
-# Author : Valentin Bilot
-
-## This notebook (Train.ipynb) and/or python script (train.py) are meant to train simple RNNs (rnn, lstm, gru) on the real book dataset (in Inputs folder) in an easy way.
-
--- SimpleRNNs/Results/RNNClass.py
+++ SimpleRNNs/Results/RNNClass.py
@@ -2,10 +2,12 @@ import torch
 from random import randint
 from utilities import chordUtil
 from utilities import dataImport
+from utilities import toyDataImport
 from utilities import plotAndTimeUtil
 from utilities.chordUtil import *
 from utilities.dataImport import *
 from utilities.plotAndTimeUtil import *
+from utilities.toyDataImport import *
 from sklearn.model_selection import train_test_split
 import os
 import numpy as np
@@ -373,3 +375,150 @@ class MYRNN(nn.Module):
             return
         else:
             return generated_sequence
+
+        
+# ToyDatasetTraining
+
+
+    def trainAndTestOnToyDataset(self, model_type, print_every, plot_every, optimizer, lossFunction, alphabet='a0', sequence_lenght=16, using_cuda=True, batch_size=128, shuffle=True, num_workers=6, hidden_size=128, num_layers=2, dropout=0.1, learning_rate=1e-4, epochs=10, use_Paul_distance=False):
+        if using_cuda:
+            print("Trying using Cuda ...")
+            use_cuda = torch.cuda.is_available()
+            if use_cuda:
+                print("OK")
+            else:
+                print("Woops, Cuda cannot be found :'( ")
+            device = torch.device("cuda:0" if use_cuda else "cpu")
+        else:
+            device = torch.device("cpu")
+            print("Using Cpu")
+        self.to(device)
+
+        start = time.time()
+
+        all_losses, test_losses, accuracy_train, accuracy_test = self.doEpochsOnToyDataset(
+            model_type, epochs, print_every, plot_every, optimizer, lossFunction, learning_rate, batch_size, shuffle, num_workers, alphabet, sequence_lenght, device, start, use_Paul_distance, training=True)
+
+        self.trainingData[0].append(self.trainingData[0][-1]+1)
+        self.trainingData[1].append(optimizer)
+        self.trainingData[2].append(lossFunction)
+        self.trainingData[3].append(all_losses)
+        self.trainingData[4].append(test_losses)
+        self.trainingData[5].append(accuracy_train)
+        self.trainingData[6].append(accuracy_test)
+        self.trainingData[7].append(plotAndTimeUtil.timeSince(start))
+        self.trainingData[8].append(learning_rate)
+        self.trainingData[9].append(use_Paul_distance)
+
+        print("Finished Training")
+
+        return
+
+    def doEpochsOnToyDataset(self, model_type, epochs, print_every, plot_every, optimizer, lossFunction, learning_rate, batch_size, shuffle, num_workers, alphabet, sequence_lenght, device, start, use_Paul_distance, training=True):
+
+        # Init Training results and monitoring data
+        all_losses = []
+        test_losses = []
+        total_loss = 0  # Reset every plot_every iters
+        test_loss = 0
+        correct_guess_train, wrong_guess_train, correct_guess_test, wrong_guess_test = 0, 0, 0, 0
+        accuracy_test = []
+        accuracy_train = []
+
+        rootname = "../../ToyDataSet/generated/"
+        filenames = os.listdir(rootname)
+        dictChord, listChord = chordUtil.getDictChord(eval(alphabet))
+
+        # Create datasets
+        files_train, files_test = train_test_split(filenames, test_size=0.7)
+        if training:
+            dataset_train = toyDataImport.ChordSeqDataset(
+                files_train, rootname, alphabet, dictChord, sequence_lenght)
+        dataset_test = toyDataImport.ChordSeqDataset(
+            files_test, rootname, alphabet, dictChord, sequence_lenght)
+
+        # Create generators
+        params = {'batch_size': batch_size,
+                  'shuffle': shuffle,
+                  'num_workers': num_workers}
+
+        # Get Paul distance matrix :
+        if use_Paul_distance:
+            M = chordsDistances.getPaulMatrix()
+            M = remapChordsToBase.remapPaulToTristan(M, alphabet)
+            distance_tensor = torch.tensor(M, dtype=torch.float).to(device)
+        else:
+            distance_tensor = 0
+
+        if training:
+            training_generator = data.DataLoader(dataset_train, **params)
+        testing_generator = data.DataLoader(dataset_test, **params)
+
+        # TODO Put more optimisers
+        if optimizer == "Adam":
+            optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+        else:
+            raise ValueError("This optimizer is unknown to me")
+
+        # TODO Put more losses
+        if lossFunction == "CrossEntropy":
+            criterion = nn.CrossEntropyLoss()
+        else:
+            raise ValueError("This loss function is unknown to me")
+
+        if training:
+            print("Start training")
+        for epoch in range(1, epochs):
+            # Training
+            if training:
+                self.train(mode=True)
+                for local_batch, local_labels in training_generator:
+                    output, loss, correct_guess, wrong_guess = self.oneBatchTrain(
+                        local_batch, local_labels, optimizer, criterion, device, use_Paul_distance, distance_tensor)
+                    total_loss += loss
+                    correct_guess_train += correct_guess
+                    wrong_guess_train += wrong_guess
+
+                if epoch % print_every == 0:
+                    accuracy = correct_guess_train / \
+                        (correct_guess_train+wrong_guess_train)
+                    print('Train : %s (%d %d%%) loss : %.4f, accuracy : %.4f%%' % (
+                        plotAndTimeUtil.timeSince(start), epoch, epoch / epochs * 100, loss, accuracy*100))
+
+                if epoch % plot_every == 0:
+                    accuracy = correct_guess_train / \
+                        (correct_guess_train+wrong_guess_train)
+                    all_losses.append(total_loss / (plot_every))
+                    accuracy_train.append(accuracy*100)
+                    total_loss = 0
+                    correct_guess_train, wrong_guess_train = 0, 0
+
+            # Testing
+            self.train(mode=False)
+            for local_batch, local_labels in testing_generator:
+                output, loss, correct_guess, wrong_guess = self.oneBatchTest(
+                    local_batch, local_labels, optimizer, criterion, device, use_Paul_distance, distance_tensor)
+                test_loss += loss
+
+                correct_guess_test += correct_guess
+                wrong_guess_test += wrong_guess
+
+            if epoch % print_every == 0:
+                accuracy = correct_guess_test / \
+                    (correct_guess_test+wrong_guess_test)
+                if training:
+                    print('Test : %s (%d %d%%) loss : %.4f, accuracy : %.4f%%' % (
+                        plotAndTimeUtil.timeSince(start), epoch, epoch / epochs * 100, loss, accuracy*100))
+
+            if epoch % plot_every == 0:
+                accuracy = correct_guess_test / \
+                    (correct_guess_test+wrong_guess_test)
+                test_losses.append(test_loss / (plot_every))
+                accuracy_test.append(accuracy*100)
+                test_loss = 0
+                correct_guess_test, wrong_guess_test = 0, 0
+
+        if training:
+            return all_losses, test_losses, accuracy_train, accuracy_test
+        else:
+            return test_losses, accuracy_test
